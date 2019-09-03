2aedd449a5b710fc548e0daef748e2cac473ce53
Bintong <yaxun_123@163.com>
2019-09-03 20:30:46 +0800

    test

--- SwiftObjLearn/CustomPopView/PopView.swift
+++ SwiftObjLearn/CustomPopView/PopView.swift
@@ -117,12 +117,6 @@ class PopView: UIView {
         
     }
     
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+  
 
 }
